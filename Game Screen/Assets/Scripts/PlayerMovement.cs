c9cd65eb3f9da043460958f5bb6a3d486c7e4478
juerokatri <C20338693@mytudublin.ie>
2021-03-08 15:04:15 +0000

    me going insane

--- Game Screen/Assets/Scripts/PlayerMovement.cs
+++ Game Screen/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed, maxSpeed;
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+
+    private void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        var moveAxis = Vector3.right * moveDir;
         
+        myRB.AddForce((Vector2)moveAxis * moveSpeed, ForceMode2D.Force);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        moveDir = context.ReadValue<float>();
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+
+        print(message:context.ReadValue<bool>());
     }
 }
