fa91883c0bb0e2d87a8efc25bbc92e099ef9d478
juerokatri <C20338693@mytudublin.ie>
2021-03-12 16:03:24 +0000

    controls and platforms

--- Game Screen/Assets/Scripts/PlayerMovement.cs
+++ Game Screen/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed, maxSpeed;
-
+    [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+ 
     private float moveDir;
     private Rigidbody2D myRB;
+    private bool canJump;
 
     private void Start()
     {
@@ -20,8 +26,21 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         var moveAxis = Vector3.right * moveDir;
+
+        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+        {
+            myRB.AddForce((Vector2) moveAxis * moveSpeed, ForceMode2D.Force);
+        }
         
-        myRB.AddForce((Vector2)moveAxis * moveSpeed, ForceMode2D.Force);
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -29,10 +48,13 @@ public class PlayerMovement : MonoBehaviour
 
         moveDir = context.ReadValue<float>();
     }
-    
+
     public void Jump(InputAction.CallbackContext context)
     {
-
-        print(message:context.ReadValue<bool>());
+        if (canJump)
+        {
+            myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
     }
 }
